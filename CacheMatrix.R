d015894bda8fc315283ed0a6029cd357b66849de
hsakarp <hsakarp@users.noreply.github.com>
2015-12-26 12:59:56 +0545

    Create CacheMatrix.R

---
+++ CacheMatrix.R
@@ -0,0 +1,35 @@
+	##  This  program constructs two functions to create a cache marix object that can be used to
+	## repeatably solve the inverse of the marix, but only
+	
+	
+	## This function creates a cacheMatrix object for an invertale matrix.
+	
+	makeCacheMatrix <- function(x = matrix()) {
+	  cachedInverse <- NULL
+	  set <- function(y) {
+	    x <<- y
+	    cachedInverse <<- NULL
+	  }
+	  get <- function() x
+	  setInverse <- function(inverse) cachedInverse <<- inverse
+	  getInverse <- function() cachedInverse
+	  list(set = set, get = get,
+	       setInverse = setInverse,
+	       getInverse = getInverse)
+	}
+	
+	
+	##This function returns the inverse of an cacheMatrix object
+	
+	cacheSolve <- function(x, ...) {
+	  ## Return a matrix that is the inverse of 'x'
+	  invFunc <- x$getInverse()
+	  if(!is.null(invFunc)) {
+	    message("getting cached data")
+	    return(invFunc)
+	  }
+	  data <- x$get()
+	  invFunc <- solve(data, ...)
+	  x$setInverse(invFunc)
+	  invFunc
+	}
